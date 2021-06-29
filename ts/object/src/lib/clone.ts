33434487ef10d5ffdaf32c9fc02f5d994cfa0da0
Maverik Minett <maverik.minett@gmail.com>
2021-06-29 07:43:28 -0400

    AGX-5 clone function

--- ts/object/src/lib/clone.ts
+++ ts/object/src/lib/clone.ts
@@ -1,7 +1,12 @@
+import { deflate, Serializer } from "./serializer"
 
 
-export function clone () {
+export function clone ( item:Object, serializer?:Serializer ) {
 
-    
+    const data = serializer ? serializer.deflate( item ) : deflate( item )
+
+    const p = Object.getPrototypeOf( item )
+
+    p.constructor 
 
 }
