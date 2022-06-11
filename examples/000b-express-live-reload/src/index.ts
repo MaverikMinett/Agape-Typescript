00636f1cd1e00b016a4ed9068358fb339469581a
Maverik Minett <maverik.minett@gmail.com>
2022-06-11 12:38:07 -0400

    example 000b-express-live-reload

---
+++ examples/000b-express-live-reload/src/index.ts
@@ -0,0 +1,115 @@
+import cors from 'cors';
+import express from 'express';
+import livereload from "livereload";
+import connectLivereload from 'connect-livereload';
+
+const PORT=3200
+
+/* Live Reload */
+const liveReloadServer = livereload.createServer();
+
+// watch files in the public directory (and send refresh signal)
+liveReloadServer.watch(`${__dirname}/../public`);   
+
+// send refresh signal to browser when server starts
+liveReloadServer.server.once("connection", () => {
+	setTimeout(() => {
+		liveReloadServer.refresh("/");
+	}, 100);
+});
+/* End Live Reload */
+
+const app = express()
+app.use(connectLivereload());
+app.use( cors({origin: '*'}) )
+app.get('', (req, res) => { res.redirect('/api')})
+
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+app.use( '/api', express.static('public') )
+app.use( '/api', express.static(pathToSwaggerUi) )
+app.use( '/swagger.json', ( req, res ) => {
+    res.send({
+        "swagger": "2.0",
+        "info": {
+          "version": "1.0.0",
+          "title": "Swagger Peestore",
+          "description": "A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification",
+          "termsOfService": "http://swagger.io/terms/",
+          "contact": {
+            "name": "Swagger API Team"
+          },
+          "license": {
+            "name": "MIT"
+          }
+        },
+        "host": "petstore.swagger.io",
+        "basePath": "/api",
+        "schemes": [
+          "http"
+        ],
+        "consumes": [
+          "application/json"
+        ],
+        "produces": [
+          "application/json"
+        ],
+        "paths": {
+          "/pets": {
+            "get": {
+              "description": "Returns all pets from the system that the user has access to",
+              "produces": [
+                "application/json"
+              ],
+              "responses": {
+                "200": {
+                  "description": "A list of pets.",
+                  "schema": {
+                    "type": "array",
+                    "items": {
+                      "$ref": "#/definitions/Pet"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "definitions": {
+          "Pet": {
+            "type": "object",
+            "required": [
+              "id",
+              "name"
+            ],
+            "properties": {
+              "id": {
+                "type": "integer",
+                "format": "int64"
+              },
+              "name": {
+                "type": "string"
+              },
+              "tag": {
+                "type": "string"
+              }
+            }
+          }
+        }
+      })
+})
+app.listen( PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}/api`)   
+})
+
+app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
