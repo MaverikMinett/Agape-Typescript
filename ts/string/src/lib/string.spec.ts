e03ef3fc76a2a826ca8ef04335980f91466e2f32
Maverik Minett <maverik.minett@gmail.com>
2023-01-21 07:58:15 -0500

    string/ add quantify function

--- ts/string/src/lib/string.spec.ts
+++ ts/string/src/lib/string.spec.ts
@@ -1,5 +1,5 @@
 import {} from "jasmine";
-import { camelize, tokenize, verbalize, classify, pluralize, titalize } from './string'
+import { camelize, tokenize, verbalize, classify, pluralize, titalize, quanitfy } from './string'
 
 
 
@@ -79,4 +79,59 @@ describe('verbalize', () => {
         expect( verbalize('foo-bar') ).toEqual('Foo bar')
     })
 
+})
+
+
+describe('quantify', () => {
+
+    let value: number|string;
+    let label: string;
+
+    beforeEach( () => {
+        value = undefined;
+        label = 'cat';
+    })
+
+    it('should pluralize the label if < 0', () => {
+        value = -1;
+        expect( quanitfy(value, label) ).toBe("-1 cats")
+    })
+
+    it('should pluralize the label if 0', () => {
+        value = 0;
+        expect( quanitfy(value, label) ).toBe("0 cats")
+    })
+
+    it('should pluralize the label if > 0 and < 1', () => {
+        value = 0.5;
+        expect( quanitfy(value, label) ).toBe("0.5 cats")
+    })
+
+    it('should not pluralize the label if 1', () => {
+        value = 1;
+        expect( quanitfy(value, label) ).toBe("1 cat")
+    })
+
+    it('should pluralize the label if 1.x', () => {
+        value = -1;
+        expect( quanitfy(value, label) ).toBe("-1 cats")
+    })
+
+    it('should pluralize the label if >= 2', () => {
+        value = 2;
+        expect( quanitfy(value, label) ).toBe("2 cats")
+
+        value = 420;
+        expect( quanitfy(value, label) ).toBe("420 cats")
+    })
+
+    it('should display formatted numbers', () => {
+        value = "05.6";
+        expect( quanitfy(value, label) ).toBe("05.6 cats")
+    })
+
+    it('should display formatted numbers', () => {
+        value = "432.00";
+        expect( quanitfy(value, label) ).toBe("432.00 cats")
+    })
 })
